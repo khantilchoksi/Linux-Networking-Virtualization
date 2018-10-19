ab01632fd4f346b9605dfca2f6ed1c1450c75f7b
Khantil Choksi <khchoksi@ncsu.edu>
2018-10-19 19:39:14 -0400

    changes

--- hw3/q1_1.py
+++ hw3/q1_1.py
@@ -5,14 +5,14 @@ from xml.dom import minidom
 def create_connection():
     conn = libvirt.open('qemu:///system')
     if not conn:
-        print "Connection to quemu faile!"
+        print ("Connection to quemu failed!")
         exit(1)
     return conn
 
 def list_vm_objects(conn):
     domainIDs = conn.listDomainsID()
     if len(domainIDs) == 0:
-        print "No active domains in current hypervisor"
+        print ("No active domains in current hypervisor")
 
     vm_id_list = [] + domainIDs
     print("Active Domain IDs: "+str(vm_id_list))
@@ -27,14 +27,15 @@ def list_vm_objects(conn):
 def list_macs(vm_list):
     for vm in vm_list:
         vm_xml = minidom.parseString(vm.XMLDesc(0))
-        interface_types = vm_xml.getElementyByTagsName('interface')
-        print('interface type: '+interface_types.getAttribute('type'))
+        interface_types = vm_xml.getElementsByTagName('interface')
+        # print('interface type: '+interface_types.getAttribute('type'))
         # interfaceNodes = interface_types.childNodes
         # for interfaceNode in interfaceNodes
         for interface in interface_types:
-            mac_tag = interface.getElementyByTagsName('mac')
-            mac_addresses = mac_tag.getAttribute('address')
-            print('mac address: '+mac_addresses)
+		print('interface type: '+interface.getAttribute('type'))
+            	mac_tag = interface.getElementsByTagName('mac')
+            	mac_addresses = mac_tag.getAttribute('address')
+            	print('mac address: '+mac_addresses)
 
 
 def list_ips():
